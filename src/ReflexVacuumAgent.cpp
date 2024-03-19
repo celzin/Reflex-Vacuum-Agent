53c8540a3f8a5642f8eba4c860b8e4c18984a477
celzin <celsovinicius4@gmail.com>
2024-03-18 21:39:58 -0300

    ...

--- src/ReflexVacuumAgent.cpp
+++ src/ReflexVacuumAgent.cpp
@@ -1,12 +1,21 @@
 #include "include/ReflexVacuumAgent.hpp"
 
+// Função que determina a ação do agente com base na sua localização e no status dessa localização
 Action ReflexVacuumAgent::reflexVacuumAgent(Location location, Status status) {
+
+    // Se o status da localização atual for 'Sujo', a ação 'Suck' é retornada
     if (status == Status::Dirty) {
         return Action::Suck;
+
+        // Se a localização atual for 'A' e estiver 'Limpa', a ação 'Direita' (B) é retornada
     } else if (location == Location::A) {
         return Action::Right;
+
+        // Se a localização atual for 'B' e estiver limpa, a ação 'Esquerda' (A) é retornada
     } else if (location == Location::B) {
         return Action::Left;
     }
+
+    // Caso nenhuma das condições, retorna 'NoOp' (sem operação).
     return Action::NoOp;
 }
